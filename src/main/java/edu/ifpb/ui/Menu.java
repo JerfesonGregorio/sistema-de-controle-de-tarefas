b99262c7cc230774f7057233c8ecd5cc6097af10
jerfeson.moreno <pro.jerfeson@gmail.com>
2025-08-15 08:41:53 -0300

    implementa primeira versao de UI

---
+++ src/main/java/edu/ifpb/ui/Menu.java
@@ -0,0 +1,6 @@
+package edu.ifpb.ui;
+
+public interface Menu {
+    void show();
+    void handleInput(String input);
+}
---
+++ src/main/java/edu/ifpb/ui/MenuUI.java
@@ -0,0 +1,22 @@
+package edu.ifpb.ui;
+
+import java.util.Scanner;
+
+public class MenuUI {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final Menu menu;
+
+    public MenuUI(Menu menu) {
+        this.menu = menu;
+    }
+
+    public void start() {
+        String input;
+        do {
+            menu.show();
+            input = scanner.nextLine();
+            menu.handleInput(input);
+        } while (!input.equals("o"));
+    }
+}
